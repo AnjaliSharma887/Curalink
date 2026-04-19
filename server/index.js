f8a0e393c7d7aebe07dc34ec24a512b7949213e7
Anjali Sharma <sharmaanjali9734@gmail.com>
2026-04-20 04:29:18 +0530

    Fix CORS

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://curalink.vercel.app', '*']
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
