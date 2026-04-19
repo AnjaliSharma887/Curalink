ac787991dafc34dd72a4284aa250b19844bb5f6e
Anjali Sharma <sharmaanjali9734@gmail.com>
2026-04-19 22:35:13 +0530

    Update API URL for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 export const sendResearchQuery = async (queryData) => {
   const response = await axios.post(`${API_BASE}/research`, queryData);
